653f13a732b56f1f74b16304e219c2fedd99c184
MHD_ISA <166327824+Mhd-Isa@users.noreply.github.com>
2024-06-19 01:43:30 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -18,13 +18,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/muhammadisa8784/Sourcodevps/main/bot/bot.zip
+wget https://raw.githubusercontent.com/Mhd-Isa/vpn-autoscript/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/muhammadisa8784/Sourcodevps/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/Mhd-Isa/vpn-autoscript/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
